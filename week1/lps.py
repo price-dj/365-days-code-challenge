8b53c39672136612458c779673c40df070b75c2a
price-dj <dprice2@student.unimelb.edu.au>
2023-01-14 18:09:35 +1100

    The basic slow version now returns a list of all the longest palindromic strings

--- week1/lps.py
+++ week1/lps.py
@@ -21,16 +21,51 @@ def longest_palindrome_substring_slow(s: str):
             and s_prime[centre + radius + 1] == s_prime[centre - radius + 1]:
 
             radius += 1
-            #print(radius)
 
         # Save the radius of the longest palindrome in the array
         palindrome_radii[centre] = 2 * radius
 
         centre += 1
 
-    print(palindrome_radii)
-    longest_palindrome_in_s_prime = max(palindrome_radii)
-    longest_palindrome_in_s = int((longest_palindrome_in_s_prime-1) / 2)
+    palindrome_radii = (palindrome_radii - 1) // 2
+    # for s_prime get all max radii and indexes
+    centres = [centre for centre, item in enumerate(palindrome_radii) if item == max(palindrome_radii)]
 
-    return longest_palindrome_in_s
+    lps_results = []
 
+    for centre in centres:
+        radius = int(palindrome_radii[centre])
+        lps_prime = s_prime[centre - radius + 1: centre + radius + 1]
+        lps = lps_prime.strip('|').split('|')
+        lps = ''.join(lps)
+        lps_results.append(lps)
+
+    return lps_results
+
+
+# def longest_palindrome_substring_manacher(s: str):
+#     """
+#
+#     :param s:
+#     :return:
+#     """
+#
+#     s_prime = '|' + '|'.join(list(s)) + '|'
+#     palindrome_radii = np.zeros(len(s_prime))
+#
+#     centre = 0
+#     radius = 0
+#
+#     while centre < len(s_prime):
+#         # At the start of the loop, Radius is already set to a lower-bound for the longest radius.
+#         # In the first iteration, Radius is 0, but it can be higher on later iterations.
+#
+#         # Determine the longest palindrome starting at Center-Radius and going to Center+Radius
+#
+#         while centre - radius + 1 >= 0 \
+#                 and centre + radius + 1 < len(s_prime) \
+#                 and s_prime[centre + radius + 1] == s_prime[centre - radius + 1]:
+#             radius += 1
+#
+#         # Save the radius of the longest palindrome in the array
+#         palindrome_radii[centre] = radius
