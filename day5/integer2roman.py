0478b680992338485f7d3f8f7230d59914e5c6ea
price-dj <dprice2@student.unimelb.edu.au>
2023-01-18 17:33:21 +1100

    Day 5 challenge, convert decimal integer 1<=i<=3999 to roman numeral

--- day5/integer2roman.py
+++ day5/integer2roman.py
@@ -11,7 +11,7 @@ def integer2roman(number: int):
     """
     decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
-    roman_numerals = {1 : "I" , 4 : "IV", 5: "V", 9 : "IX", 10 : "X",40 : "XL", 50 : "L",
+    roman_numerals = {1 : "I" , 4 : "IV", 5: "V", 9 : "IX", 10 : "X", 40 : "XL", 50 : "L",
                      90 : "XC", 100 : "C", 400 : "CD", 500 : "D", 900 : "CM", 1000 : "M"}
 
     roman_numeral_result = []
@@ -37,4 +37,4 @@ def integer2roman(number: int):
 
 
 if __name__ == "__main__":
-    print(integer2roman(2023))
+    print(integer2roman(1050))
